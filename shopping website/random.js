d7b8a84d60d355c89108dccf2253b94518e59d7b
ashishpipaliya30 <ashishpipaliya30@gmail.com>
2024-09-28 18:59:26 +0530

    set header section completely

--- shopping website/random.js
+++ shopping website/random.js
@@ -3,19 +3,11 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    cartItem.classList.remove('active');
     searchForm.classList.remove('active');
-}
-// For click 3 line cart-item button and get opetions
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
-// For click 3 line cart-item button and get opetions
+// for cart-item, click cart button and get options
 let cartItem = document.querySelector('.cart-items-cntainer');
 
 document.querySelector('#cart-btn').onclick = () =>{
@@ -24,6 +16,16 @@ document.querySelector('#cart-btn').onclick = () =>{
     searchForm.classList.remove('active');
 }
 
+// for search, click search button and get search options
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+        searchForm.classList.toggle('active');
+        navbar.classList.remove('active');
+        cartItem.classList.remove('active');
+}
+
+// for we open one menu then remove other menu
 window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
